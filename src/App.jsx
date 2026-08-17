73b9789ac9d3101a94b490f39709750fa2b8a0a9
bol0 <kubles02@gmail.com>
2026-08-17 15:32:49 +0200

    clean-up

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { z } from "zod";
 
-import "./App.css";
 import FormSummary from "./components/FormSummary/FormSummary";
 import Form from "./components/Form/Form";
 
